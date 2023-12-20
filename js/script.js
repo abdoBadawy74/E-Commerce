fd84a92d43729fccabc4cb8f50a648cc21ee571e
abd074 <badawyabdulrahman099@gmail.com>
2023-12-20 23:45:44 +0200

    add products with js

--- js/script.js
+++ js/script.js
@@ -19,8 +19,67 @@ if (username) {
 logOutBtn.addEventListener("click", logout);
 
 function logout() {
-    window.localStorage.clear();
-    setTimeout(() => {
-      window.location = "../register.html";
-    }, 1500);
-  }
+  window.localStorage.clear();
+  setTimeout(() => {
+    window.location = "../register.html";
+  }, 1500);
+}
+
+// define products
+
+let productsDom = document.querySelector(".products");
+
+let products = [
+  {
+    id: 1,
+    title: "headphone",
+    size: "large",
+    imageUrl: "./images/headphone.jpg",
+  },
+  {
+    id: 2,
+    title: "Laptop",
+    size: "small",
+    imageUrl: "./images/laptop.jpg",
+  },
+  {
+    id: 3,
+    title: "keyboard",
+    size: "medium",
+    imageUrl: "./images/keyboard.jpg",
+  },
+  {
+    id: 4,
+    title: "tablet",
+    size: "meduim",
+    imageUrl: "./images/tablet.jpg",
+  },
+  {
+    id: 5,
+    title: "pc",
+    size: "large",
+    imageUrl: "./images/pc.jpeg",
+  },
+];
+
+function drawProductUI() {
+  let productsUI = products.map((item) => {
+    return `<div class="product-item">
+      <img src="${item.imageUrl}" class="product-img" alt="image" />
+      <div class="product-desc">
+        <h2>${item.title}</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur
+        </p>
+        <span>size: ${item.size}</span>
+      </div>
+      <div class="product-actions">
+        <button class="add-to-cart">Add To Cart</button>
+        <i class="fa-regular fa-heart favorite"></i>
+      </div>
+    </div>`;
+  });
+  productsDom.innerHTML = productsUI.join('');
+}
+
+drawProductUI();
